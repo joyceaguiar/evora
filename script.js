8a542b08f3d8cdc3b9f969e5548c8ed1bdb4de31
Joyce Aguiar <joyceaguiar777@gmail.com>
2025-02-25 14:15:03 -0300

    final final

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+function showLove() {
+    var messageElement = document.getElementById("message");
+    messageElement.innerHTML = "Eu te amo, amiga! 💖";
+    messageElement.style.display = "block";
+}
